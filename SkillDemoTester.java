b544c58946a4709534dbd4d720ff8138633c9e0d
ValerieNguy <75493729+ValerieNguy@users.noreply.github.com>
2022-02-02 21:30:45 -0800

    added libraries and failed test

---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    
+    @Test
+    public void multiplication() {
+        assertEquals(4, SkillDemo.multiply(2,3)); //should fail
+    }
+}
